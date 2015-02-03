c984ad8c24afee8b493c2f362a80c60499f254d8
Magambo Gatete Elie <eliemagambo@gmail.com>
2015-02-03 17:11:29 +0900

    Added variables

---
+++ embbed_variables.py
@@ -0,0 +1,16 @@
+myname='Elie Magambo'
+my_age='29' #Depends on the date you re reading this
+my_height=181 #cm
+my_weight=75 #kg
+my_eyes='Black'#not sure if true
+my_teeth="eeew"
+my_hair="Curly black"
+
+print "lets talk about %s"%myname
+print "He's %d cm tall"%my_height
+print "He's %d kg heavy"%my_weight
+print "Actually that's not too heavy"
+print "he gots %s eyes and %s hair"%(my_eyes,my_hair)
+print "His teeth are usually %s depending on the coffee."%my_teeth
+#this line is tricky of course i got it right
+print "If I add %d, %d,and %d I get %d "%(int(my_age),my_height,my_weight,int(my_age)+my_height+my_weight)
