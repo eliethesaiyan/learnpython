906c4ef0bceb62029d85392989836baea70603f4
Magambo Gatete Elie <eliemagambo@gmail.com>
2015-02-03 16:24:35 +0900

    initial commit

---
+++ comment_pound_char.py
@@ -0,0 +1,7 @@
+#A comment ,this is so you can read your program later
+#   Anything after the # is ignored by python
+
+
+print "I could have code like this " #And the comment after
+
+
---
+++ hello_world.py
@@ -0,0 +1,6 @@
+print "Hello World"
+print "Hello Again"
+print "I like typing this "
+print 'single quotes'
+print "Embedding 'single quotes in double quotes'"
+print 'Embedding "double quotes in single quotes"'
